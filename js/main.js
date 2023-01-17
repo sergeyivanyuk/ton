f90b7b05eaa4723a52018a2e6706d99c00f35063
Sergey <sergeyivanyuk89@gmail.com>
2023-01-17 20:19:32 +0400

    add js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const  OpenModalProfile = document.querySelector('.modal__profile-open'),
+  const  OpenModalProfile = document.querySelectorAll('.item'),
          ModalProfile = document.querySelector('.modal__profile');
          CloseModal = ModalProfile.querySelector('.modal__close'),
 
 
-         OpenModalProfile.addEventListener('click', (e) => {
-            e.preventDefault();
-            ModalProfile.classList.add('active');
+         OpenModalProfile.forEach(item => {
+            item.addEventListener('click', (e) => {
+               e.preventDefault();
+               ModalProfile.classList.add('active');
+            });
+            
          });
 
          CloseModal.addEventListener('click', (e) => {
